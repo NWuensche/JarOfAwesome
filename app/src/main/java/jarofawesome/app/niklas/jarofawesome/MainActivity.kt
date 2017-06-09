79d5efac9b602866fb27516cdf28f663ebb4b881
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-06-09 13:48:56 +0200

    Add Kotlin extensions

--- app/src/main/java/jarofawesome/app/niklas/jarofawesome/MainActivity.kt
+++ app/src/main/java/jarofawesome/app/niklas/jarofawesome/MainActivity.kt
@@ -1,23 +1,20 @@
 package jarofawesome.app.niklas.jarofawesome
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import jarofawesome.app.niklas.jarofawesome.R.id.action_settings
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
@@ -34,10 +31,8 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
 
-
-        if (id == R.id.action_settings) {
+        if (item.itemId == action_settings) {
             return true
         }
 
