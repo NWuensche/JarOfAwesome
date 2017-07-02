d11e5c2bb4b953a4645db1e981377655a017ef10
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-07-02 15:44:00 +0200

    Make TextView to Button and delete menu

--- app/src/main/java/jarofawesome/app/niklas/jarofawesome/MainActivity.kt
+++ app/src/main/java/jarofawesome/app/niklas/jarofawesome/MainActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import jarofawesome.app.niklas.jarofawesome.R.id.action_settings
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import org.jetbrains.anko.alert
@@ -51,18 +50,6 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        if (item.itemId == action_settings) {
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
     }
 
     fun saveToDB(newRecord: String) {
